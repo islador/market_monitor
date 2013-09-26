c5e8c0b0711802749657818122ed48fd622c7f92
islador <luke.isla@gmail.com>
2013-09-26 16:52:36 -0700

    Added static pages for home and help, got routes working for localhost:3000/home instead of localhost:3000/static_pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 MarketMonitor::Application.routes.draw do
+  #get "static_pages/home"
+
+  #get "static_pages/help"
+
+  match "/home", to: 'static_pages#home'
+  match "/help", to: 'static_pages#help'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
