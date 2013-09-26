cbc2077f651a8f110ab8e7ebaa07739a1b83b26f
islador <luke.isla@gmail.com>
2013-09-25 20:39:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketMonitor::Application.config.secret_token = 'fc19e1574efc32a7ff74973bacc4711ad79a54c7eac1dbc8b8e38c3623e901259aec04071256b8e8c557cad52aa1981e6fe83422e6cc3da96053aa1c631d5c7e'
