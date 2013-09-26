b4e401108e42612f9c804811c5feb350513510c3
islador <luke.isla@gmail.com>
2013-09-26 16:53:40 -0700

    Swapped the root to static_pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ MarketMonitor::Application.routes.draw do
 
   #get "static_pages/help"
 
+  root to: 'static_pages#home'
+
   match "/home", to: 'static_pages#home'
   match "/help", to: 'static_pages#help'
 
