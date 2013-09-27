543715345f9aaec4b17a35a7ebd43d0c5863fd6b
islador <luke.isla@gmail.com>
2013-09-26 17:16:43 -0700

    Created a users controller and integration test page for it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 MarketMonitor::Application.routes.draw do
+  get "users/new"
+
   #get "static_pages/home"
 
   #get "static_pages/help"
 
   root to: 'static_pages#home'
+
+  match "/signup", to: 'users#/new'
 
   match "/home", to: 'static_pages#home'
   match "/help", to: 'static_pages#help'
