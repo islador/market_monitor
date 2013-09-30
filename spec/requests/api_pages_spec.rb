e183287b2646e8264b026e1f54adff03f849fc6b
islador <luke.isla@gmail.com>
2013-09-30 01:58:56 -0700

    Added specs for the newcorporationapi page. Including enrollment testing spec. Note: new page names needed badly.

--- spec/requests/api_pages_spec.rb
+++ spec/requests/api_pages_spec.rb
@@ -7,7 +7,7 @@ describe "APIPages" do
 	describe "newcharacterapi page" do
 		let(:user) {FactoryGirl.create(:user)}
 
-		describe "for non-signed-in users" do
+		describe "non-signed-in users" do
 			before {visit newcharacterapi_path}
 
 			it "should redirect to the signin page" do
@@ -15,29 +15,55 @@ describe "APIPages" do
 			end
 		end
 
-		describe "for signed-in users" do
+		describe "signed-in users" do
 			before do
 				sign_in user
 				visit newcharacterapi_path
 			end
 
-			it {should have_title(full_title('Create new Character API'))}
+			it {should have_title(full_title('Enroll new Character API'))}
 			#it {should have_selector('label', text: 'Key ID')}
 			#it {should have_selector('label', text: 'Verification Code')}
+		end
+	end
+
+	describe "newcorporationapi page" do
+		let(:user) {FactoryGirl.create(:user)}
+
+		describe "non-signed-in users" do
+			before {visit newcorporationapi_path}
+
+			it "should redirect to the signin page" do
+				should have_title(full_title('Sign In'))
+			end
+		end
+
+		describe "signed-in users" do
+			before do
+				sign_in user
+				visit newcorporationapi_path
+			end
+
+			it {should have_title(full_title('Enroll new Corporation API'))}
+
+			describe "should be able to enroll APIs" do
+				before do
+					fill_in 'Key ID', with: "123456789"
+					fill_in "Verification Code",	with: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+					click_button "Submit New Key"
+				end
+				it "should redirect to the api list" do
+					should have_title(full_title('API List'))
+				end
+			end
 		end
 	end
 
 	describe "api list page" do
 		let(:user) {FactoryGirl.create(:user)}
 		#let(:api) {FactoryGirl.create(:api, user: user)}
-		#let(:a2) {FactoryGirl.create(:corporation, user: user)}
-		
-		#before do
-		#	@a3 = user.apis.build(type: 1, key_id: "123456789", v_code: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-		#  	accessmask: 71307264, active: 1)
-		#end
 
-		describe "for non-signed-in users" do
+		describe "non-signed-in users" do
 			before {visit apilist_path}
 
 			it "should redirect to the signin page" do
@@ -46,7 +72,7 @@ describe "APIPages" do
 			end
 		end
 
-		describe "for signed-in users" do
+		describe "signed-in users" do
 			before do
 				sign_in user
 				#visit newcharacterapi_path
