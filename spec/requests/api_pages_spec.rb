d582486f538acb60fd3e646ed1a8b4126508a2f8
islador <luke.isla@gmail.com>
2013-09-30 01:00:52 -0700

    Moved fill_in lines to the before block after reading Steve's response to Starkers' post. This explains why the sign_in code would fire properly while the fill_in outside of the before blocks would not. sign_in was always used inside an before block. This is consistent with user_pages_spec in sample_app. Why didn't I notice this sooner?

--- spec/requests/api_pages_spec.rb
+++ spec/requests/api_pages_spec.rb
@@ -29,7 +29,7 @@ describe "APIPages" do
 
 	describe "api list page" do
 		let(:user) {FactoryGirl.create(:user)}
-		#let(:api) {FactoryGirl.create(:api, user: user)}
+		let(:api) {FactoryGirl.create(:api, user: user)}
 		#let(:a2) {FactoryGirl.create(:corporation, user: user)}
 		
 		#before do
@@ -50,23 +50,31 @@ describe "APIPages" do
 			before do
 				sign_in user
 				visit newcharacterapi_path
-				#visit apilist_path
+
+				fill_in 'Key ID', with: "123456789"
+				fill_in "Verification Code",	with: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+				click_button "Submit New Key"
+				visit apilist_path
 			end
 
-			it {should have_title(full_title('Create new Character API'))}
-			it {should_not have_title(full_title('Sign In'))}
-			it {should have_selector('label',	text: "Key ID")}
-			it {should have_selector('label',	text: "Verification Code")}
-			it {should have_selector('input')}
-			#fill_in "Key ID",	with: "123456789"
+			#visit apilist_path
+
+			#it {should have_title(full_title('Create new Character API'))}
+			#it {should_not have_title(full_title('Sign In'))}
+			#it {should have_selector('label',	text: "Key ID")}
+			#it {should have_selector('label',	text: "Verification Code")}
+			#it {should have_selector('input')}
+			#it {should have_link('Predefined API Key')}
+			#click_link('Predefined API Key')
+			#fill_in 'Key ID',	with: "123456789"
 			#fill_in "Verification Code",	with: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 			#click_button "Submit New Key"
 
 			#visit apilist_path
 
-			#it {should have_title(full_title('API List'))}
+			it {should have_title(full_title('API List'))}
 			#it {should have_selector('span.KeyID')}
-			#it {should have_selector('h3', text: "APIs (1)")}
+			it {should have_selector('h3', text: "APIs (1)")}
 			#it {should have_content('Key ID')}
 		end
 	end
