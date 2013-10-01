0b2f4a8a9594b53bcc4b4ab0f293b6ecbcc00865
islador <luke.isla@gmail.com>
2013-10-01 15:51:20 -0700

    Generated the basic MarketOrder model. No migrations done yet.

---
+++ db/migrate/20131001225051_create_market_orders.rb
@@ -0,0 +1,25 @@
+class CreateMarketOrders < ActiveRecord::Migration
+  def change
+    create_table :market_orders do |t|
+      t.integer :user_id
+      t.integer :api_id
+      t.integer :market_summary_id
+      t.integer :order_id
+      t.integer :station_id
+      t.integer :vol_entered
+      t.integer :vol_remaining
+      t.integer :min_volume
+      t.integer :order_state
+      t.integer :type_id
+      t.integer :reach
+      t.integer :account_key
+      t.integer :duration
+      t.decimal :escrow
+      t.decimal :price
+      t.boolean :bid
+      t.datetime :issued
+
+      t.timestamps
+    end
+  end
+end
