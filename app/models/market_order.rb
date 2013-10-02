6a7bdb0196937c07cede98837a47b2ae3b2df5fc
islador <luke.isla@gmail.com>
2013-10-02 15:27:59 -0700

    Added order_id uniqueness validation. This is necessary to enforce record uniqueness and ease updating burdens.

--- app/models/market_order.rb
+++ app/models/market_order.rb
@@ -30,8 +30,10 @@
 
 
 class MarketOrder < ActiveRecord::Base
-  attr_accessible :account_key, :api_id, :bid, :duration, :escrow, :issued, :market_summary_id, :min_volume, :order_id, :order_state, :price, :reach, :station_id, :type_id, :user_id, :vol_entered, :vol_remaining
+  attr_accessible :account_key, :api_id, :bid, :duration, :escrow, :issued, :market_summary_id, :min_volume, :order_id, :order_state, :price, :reach, :station_id, :type_id, :user_id, :vol_entered, :vol_remaining, :char_id
 
   belongs_to :apis
   belongs_to :users
+
+  validates :order_id, presence: true, uniqueness: true
 end
