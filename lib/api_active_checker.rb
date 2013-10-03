ec84a97205079149225379d8e7f7709e9368ebf2
islador <luke.isla@gmail.com>
2013-10-03 00:55:03 -0700

    No methods written, just an empty file right now.

---
+++ lib/api_active_checker.rb
@@ -0,0 +1,8 @@
+module ApiActiveChecker
+	# do some logic here to see if the API is live.
+	# http://wiki.eve-id.net/APIv2_Eve_ErrorList_XML looks like
+	# a good place to start to define that logic.
+	def checkactive(id)
+	end
+
+end
