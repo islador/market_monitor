4f7c2d5f6a67e3d3afdcf3dd423b68e1daf4bc4b
islador <luke.isla@gmail.com>
2013-10-02 21:18:15 -0700

    Created an input_orders method in the corpmarketorders module. Included CorpMarketOrders in CacheTimesController and arranged to fire input_orders when a timer is finished with a call_type of 2. This will in turn fire the update method in CorpMarketOrders should it encounter a pre-existing order_id.

--- app/controllers/cache_times_controller.rb
+++ app/controllers/cache_times_controller.rb
@@ -1,4 +1,15 @@
+# call_type translations
+# | Code | Translation |
+# |:----:|:-----------:|
+# | 1 | Api Status |
+# | 2 | Corp Market Order |
+# | 3 | Corp Wallet Transactions |
+# | 4 | Character Market Order |
+# | 5 | Character Wallet Transactions |
+
 class CacheTimesController < ApplicationController
+	include ApiActiveChecker
+	include CorpMarketOrders
 
 	def checktimes
 
@@ -11,7 +22,17 @@ class CacheTimesController < ApplicationController
 
 			#Evaluate if the cache timer has expired or not.
 			if CacheTimes.find_by_id(n).cached_time < DateTime.now
-				CacheTimes.find_by_id(n).call_type
+
+				# If call_type is 1, fire off ApiActiveChecker's checkactive method.
+				if CacheTimes.find_by_id(n).call_type == 1
+					ApiActiveChecker.checkactive(CacheTimes.find_by_id(n).api_id)
+				end
+
+				# If call_type is 2, fire off CorpMarketOrders 
+				if CacheTimes.find_by_id(n).call_type == 2
+					CorpMarketOrders.input_orders(CacheTimes.find_by_id(n).api_id)
+				end
+					
 
 	end
 end
--- lib/corp_market_orders.rb
+++ lib/corp_market_orders.rb
@@ -1,6 +1,5 @@
 module CorpMarketOrders
-	def Update(id)
-		@api = id #
+	def update(id)
 
 		#Assemble a hash for use in updating existing orders.
 		@updatehash = {"vol_remaining" => nil, "order_state" => nil, "escrow" => nil, "price" => nil}
@@ -38,6 +37,61 @@ module CorpMarketOrders
 
 				#Update the existing order.
 				MarketOrder.find_by_order_id(o.orderID).update_attributes(@updatehash)
+			end
+		end
+	end
+
+	def input_orders(id)
+		#Assemble a hash for use in inserting into the database.
+		@temphash = {"user_id" => nil, "api_id" => nil, "market_summary_id" => nil, "order_id" => nil, "char_id" => nil, "station_id" => nil, "vol_entered" => nil, "vol_remaining" => nil, "min_volume" => nil, "order_state" => nil, "type_id" => nil, "reach" => nil, "account_key" => nil, "duration" => nil, "escrow" => nil, "price" => nil, "bid" => nil, "issued" => nil }
+		#Extract the active status from the database.
+		@active = Api.find_by_id(id)
+
+		#Check for activity to ensure dead APIs aren't hammered.
+		if @active == 1
+
+			#Assemble the API for the EVE Gem.
+			api = Eve::API.new(:key_id => Api.find_by_id(id).key_id, :v_code => Api.find_by_id(id).v_code)
+
+			#Load Characters to retrieve character IDs
+			result = api.account.characters
+
+			#select the user from the API assocation
+			@user = User.find_by_id(Api.find_by_id(id).user_id)
+				
+			# Build and make the API Call
+			api.set( :character_id => result.characters[0].character_id)
+			result = api.corporation.market_orders
+
+			#set the api_id before looping as it is static.
+					@temphash["api_id"] = id 
+
+			#Iterate through each returnd order.
+			result.each do |o|
+				#Check for existing order, if found, execute the update method.
+				if MarketOrder.find_by_order_id(o.orderID) != nil
+					update(id)			
+				else
+					#Load each order attribute into a hash for easy database insertion.
+					@temphash["order_id"] = o.orderID
+					@temphash["char_id"] = o.charID
+					@temphash["station_id"] = o.stationID
+					@temphash["vol_entered"] = o.volEntered
+					@temphash["vol_remaining"] = o.volRemaining
+					@temphash["min_volume"] = o.minVolume
+					@temphash["order_state"] = o.orderState
+					@temphash["type_id"] = o.typeID
+					@temphash["reach"] = o.range
+					@temphash["account_key"] = o.accountKey
+					@temphash["duration"] = o.duration
+					@temphash["escrow"] = o.escrow
+					@temphash["price"] = o.price
+					@temphash["bid"] = o.bid
+					@temphash["issued"] = o.issued
+
+					#Insert into the database
+					@user.market_orders.build(@temphash)
+				end
 			end
 		end
 	end
