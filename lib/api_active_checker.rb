55360f8d51dca32839976f792b4f0127b91310f4
islador <luke.isla@gmail.com>
2013-10-03 22:45:17 -0700

    Added basic CacheTimes update machinery to ApiActiveChecker. This is not the complete method but is necessary to reduce erroneous calls to the server.

--- lib/api_active_checker.rb
+++ lib/api_active_checker.rb
@@ -3,7 +3,19 @@ module ApiActiveChecker
 	# http://wiki.eve-id.net/APIv2_Eve_ErrorList_XML looks like
 	# a good place to start to define that logic.
 	def self.checkactive(api_id, cache_time_id)
+		@user_id = api_id.user_id
+		@api = Api.find_by_id(api_id)
+
 		puts "Unfinished ApiActiveChecker.checkactive fired at #{DateTime.now}"
+
+		api = Eve::API.new(:key_id => @api.key_id, :v_code => @api.v_code)
+    	result = api.account.apikeyinfo
+
+		@newTimer = CacheTimes.new(user_id: @user.id, api_id: api_id, call_type: 1, cached_time: result.cachedUntil)
+		puts @newTimer
+		@newTimer.save
+
+		CacheTimes.delete(cache_time_id)
 	end
 
 end
