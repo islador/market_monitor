cd2e73ee42cea448d37d75415dbc008717018dd7
islador <luke.isla@gmail.com>
2013-10-05 01:19:45 -0700

    Wrote basic tests for the three currently active modules, ApiActiveChecker, CorpMarketOrder and CharMarketOrder. The specs test that new CacheTimes are created and the invoking CacheTime is deleted. Full function tests are still necessary.

--- spec/modules/cache_timers_spec.rb
+++ spec/modules/cache_timers_spec.rb
@@ -19,27 +19,54 @@ describe "CacheTimers" do
 		end
 	end
 
-	#forcefully evaluated to ensure the models are active.
+	#forcefully evaluated to ensure the user is active.
 	let!(:user) {FactoryGirl.create(:user)}
-	let!(:api) {FactoryGirl.create(:api)}
-	let!(:timer) {FactoryGirl.create(:cache_times)}
 
-	describe "CacheTimers.checktimes" do
+	describe "should fire ApiActiveChecker" do
+		#Force load a CacheTimes model for this test.
+		let!(:timer) {FactoryGirl.create(:cache_times)}
+		
+		#Instantiate a copy of DummyClass to test the CacheTimers machinery.
 		$derp = DummyClass.new
-
-		it "ApiActiveChecker.checkactive should fire" do
-			expect($derp.checktimes).not_to change(CacheTimes, :count)
-			expect(timer.id).to be_nil
+		it "should find call_type 1 and fire ApiActiveChecker.checkactive" do
+			#Define which cassette to use to avoid testing CCP's APIs
+			expect do
+				VCR.use_cassette 'modules/api_active_checker' do
+					#Call the method
+					$derp.checktimes
+				end
+				#Test that ApiActiveChecker.checkactive builds a new CacheTimes model.
+			end.not_to change(CacheTimes, :count)
+			#Test that the CacheTimes model used was destroyed by CacheTimers.checktimes
+			expect(CacheTimes.find_by_id(timer.id)).to be_nil
 		end
 	end
 
-	describe "CacheTimers should fire CorpMarketOrder" do
-		let!(:corporder) {FactoryGirl.create(:corpMarketOrder)}
-		it "should find call_type 2 and fire CorpMarketOrder.input_orders" do
-			#@dummy
-			#expect {@dummy}.not_to change(CacheTimes, :count)
-			#expect{corporder.id}.to be_nil
+	describe "should fire CharMarketOrder" do
+		let!(:corpordertimer) {FactoryGirl.create(:corpMarketOrder)}
+
+		$derp = DummyClass.new
+		it "should find call_type 2 and fire CharMarketOrder.input_orders" do
+			expect do
+				VCR.use_cassette 'modules/corp_market_order' do
+					$derp.checktimes
+				end
+			end.not_to change(CacheTimes, :count)
+			expect(CacheTimes.find_by_id(corpordertimer.id)).to be_nil
 		end
 	end
 
+	describe "should fire CharMarketOrder" do
+		let!(:charordertimer) {FactoryGirl.create(:charMarketOrder)}
+
+		$derp = DummyClass.new
+		it "should find call_type 4 and fire CharMarketOrder.input_char_orders" do
+			expect do
+				VCR.use_cassette 'modules/char_market_order' do
+					$derp.checktimes
+				end
+			end.not_to change(CacheTimes, :count)
+			expect(CacheTimes.find_by_id(charordertimer.id)).to be_nil
+		end
+	end
 end
