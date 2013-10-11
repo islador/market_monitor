8733938442a8ab80488069604ae912a0305b12a5
islador <luke.isla@gmail.com>
2013-10-10 17:26:31 -0700

    Completed the MISBuilder.build function, it now properly creates new MIS and assigns unassigned orders to previously created MIS.

--- lib/mis_builder.rb
+++ lib/mis_builder.rb
@@ -31,7 +31,7 @@ module MISBuilder
 		50.times{FactoryGirl.create(:market_order, user: @@user2, api: @@corpapi2, :set_station => rand(1000..1020), :set_type => rand(1000..1200), :set_char => 600750)}
 		puts "Built out 50 MOs for User2 CorpApi2"
 
-		#build(@@user1.id)
+		build(@@user1.id)
 	end
 
 	def self.build(user_id)
@@ -43,6 +43,7 @@ module MISBuilder
 		
 		#Assemble a hash for use in building new MIS
 		temphash = {"type_id" => nil, "station_id" => nil, "char_id" => nil, "entity" => nil, "bid" => nil}
+		#Assemble the user and apilist arrays.
 		user = User.where('id = ?', user_id)
 		apilist = Api.where('user_id = ?', user_id)
 
@@ -50,16 +51,17 @@ module MISBuilder
 		apilist.each do |api|
 			#Set the API's entity value.
 			temphash["entity"] = api.entity
-			#Query for orders matching the current API, User and not already in a MIS
+			#Query for orders that are not in a MIS but belong to the current API and User.
 			returned_orders = MarketOrder.where('user_id = :userid AND api_id = :apiid AND market_item_summary_id IS NULL', {userid: user_id, apiid: api.id})
-
+			#Iterate through each returned order.
 			returned_orders.each do |ro|
 				#Query for any MIS that match the current order.
-				mis = MarketItemSummaries.where('user_id = :userid AND entity = :entity AND type_id = :mo_type_id AND station_id = :mo_station_id AND char_id = :mo_char_id AND bid = :mo_bid', {userid: user_id, entity: api.entity, mo_type_id: ro.type_id, mo_station_id: ro.station_id, mo_char_id: ro.char_id, mo_bid: ro.bid})
+				mis = MarketItemSummary.where('user_id = :userid AND entity = :entity AND type_id = :mo_type_id AND station_id = :mo_station_id AND char_id = :mo_char_id AND bid = :mo_bid', {userid: user_id, entity: api.entity, mo_type_id: ro.type_id, mo_station_id: ro.station_id, mo_char_id: ro.char_id, mo_bid: ro.bid})
 
 				#Check if an empty array is returned or if it contains a model.
 				if mis.empty?
-					#if it does not contain a model
+					#if it does not contain a model query for the order.
+					#this is done because I am unsure how the index will behave if I use the ro instead of a new variable.
 					order = MarketOrder.where('id = ?', ro.id)
 					#assemble a hash
 					temphash["type_id"] = order[0].type_id
@@ -68,7 +70,7 @@ module MISBuilder
 					temphash["bid"] = order[0].bid
 
 					#and build a new MIS.
-					new_mis = user.MarketItemSummaries.build(temphash)
+					new_mis = user[0].market_item_summaries.build(temphash)
 					new_mis.save
 				else
 					#If it contains a model, associate the MO with the matching MIS.
@@ -103,5 +105,11 @@ module MISBuilder
 		#Trigger: Order and Wallet APIs
 		#Attributes touched:
 		#It's possible to use database aggregate functions to generate the average prices. Average % markup up is looking like it will still require logic. Not sure on that just yet though.
+
+		mis = MarketItemSummary.where('id = ?', market_item_summary_id)
+		rmo = MarketOrder.where('market_item_summary_id = ?', market_item_summary_id)
+
+		#Client.select('avg(price) as price, type_id, station_id, char_id').group('type_id').group('station_id').group('char_id').where('user_id = 8')
+		MarketOrder.select('avg(price)as price').group('market_item_summary_id = ?', market_item_summary_id)
 	end
 end
