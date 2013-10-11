3e162edc2feaff6362e9c2dd266f7be5b6fa8461
islador <luke.isla@gmail.com>
2013-10-10 17:24:04 -0700

    Renamed and updated the model file for MarketItemSummaries to match with conventions and the new table name.

--- app/models/market_item_summary.rb
+++ app/models/market_item_summary.rb
@@ -18,7 +18,7 @@
 #  bid                    :boolean
 #
 
-class MarketItemSummaries < ActiveRecord::Base
+class MarketItemSummary < ActiveRecord::Base
   attr_accessible :average_percent_markup, :average_purchase_price, :average_sale_price, :char_id, :entity, :station_id, :total_vol_entered, :total_vol_remaining, :type_id, :user_id, :bid
 
   belongs_to :user
