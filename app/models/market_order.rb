4d5a93a87243301d6977dabff043d590c6c85805
islador <luke.isla@gmail.com>
2013-10-10 17:25:34 -0700

    Updated the market_summary_id attribute name to match the new column name: market_item_summary_id. Annotations were also updated.

--- app/models/market_order.rb
+++ app/models/market_order.rb
@@ -2,27 +2,27 @@
 #
 # Table name: market_orders
 #
-#  id                :integer          not null, primary key
-#  user_id           :integer
-#  api_id            :integer
-#  market_summary_id :integer
-#  order_id          :integer
-#  station_id        :integer
-#  vol_entered       :integer
-#  vol_remaining     :integer
-#  min_volume        :integer
-#  order_state       :integer
-#  type_id           :integer
-#  reach             :integer
-#  account_key       :integer
-#  duration          :integer
-#  escrow            :decimal(, )
-#  price             :decimal(, )
-#  bid               :boolean
-#  issued            :datetime
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  char_id           :integer
+#  id                     :integer          not null, primary key
+#  user_id                :integer
+#  api_id                 :integer
+#  market_item_summary_id :integer
+#  order_id               :integer
+#  station_id             :integer
+#  vol_entered            :integer
+#  vol_remaining          :integer
+#  min_volume             :integer
+#  order_state            :integer
+#  type_id                :integer
+#  reach                  :integer
+#  account_key            :integer
+#  duration               :integer
+#  escrow                 :decimal(, )
+#  price                  :decimal(, )
+#  bid                    :boolean
+#  issued                 :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  char_id                :integer
 #
 
 # Market Orders to Model Map
