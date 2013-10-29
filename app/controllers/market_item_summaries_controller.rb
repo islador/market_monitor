e34f29eeaf9d6053a31e531fd63fcae97a914288
islador <luke.isla@gmail.com>
2013-10-28 17:01:54 -0700

    Began prototyping dynamic MIS table generation code on show_mis.html.erb. Completed MIS row generation, next is component MO generation.

--- app/controllers/market_item_summaries_controller.rb
+++ app/controllers/market_item_summaries_controller.rb
@@ -3,9 +3,14 @@ class MarketItemSummariesController < ApplicationController
 		if signed_in?
 	      #@apis = current_user.apis.paginate(page: params[:page]) #doesn't appear to be necessary.
 	      render 'show_msi'
+	      #@mis = MarketItemSummary.where('user_id = ?', user_id)
 	    else
 	      flash[:error] = "You must be signed in to view this page."
 	      redirect_to signin_path
 	    end
+	end
+
+	def populate_tables(user_id)
+		@mis = MarketItemSummary.where('user_id = ?', user_id)
 	end
 end
