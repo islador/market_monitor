0a8786dda5234e9fae4ef1f95372a3814acea468
islador <luke.isla@gmail.com>
2013-10-30 20:52:15 -0700

    Learned JQuery to write a function that pulls the value of the selected option from each selected drop down for use in building a query. Began learning AJAX for use in ajax calls against the MarketItemSummariesController#filter method.

--- app/assets/javascripts/market_item_summaries.js
+++ app/assets/javascripts/market_item_summaries.js
@@ -1,3 +1,37 @@
 //# Place all the behaviors and hooks related to the matching controller here.
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+function dropdownCheck()
+{
+	//var ss = document.getElementById("station_select").selectedIndex;
+	//var ss = document.getElementById("station_select").value;
+	//var ls = document.getElementById("listing_character").value;
+	//var ow = document.getElementById("owner").value;
+	//var ty = document.getElementById("type").value;
+	//alert(ss + " " + ls + " " + ow + " " + ty);
+	//$("#filter").click(function(){
+    //    $.ajax({
+    //    	url: "marketsummaries/filter", type: "GET"
+    //    	data: {station_select : ss, listing_character : ls, owner : ow, type : ty}
+    //    });
+    //});
+}
+
+$(document).ready(function(){
+	$("#station_select").change(function(){
+		var ss = $("#station_select").val();
+		var ls = $("#listing_character").val();
+		var ow = $("#owner").val();
+		var ty = $("#type").val();
+	    $.ajax({
+	    	url: "marketsummaries/filter", type: "GET",
+	    	data: { station_id: ss, 
+	    		listing_character_id: ls, 
+	    		owner_id: ow, 
+	    		type: ty }
+	    	//data: { ss, ls, ow, ty }
+	    });
+	    alert("Unobtrusive!" + ss + ls + ow + ty);
+	});
+});
