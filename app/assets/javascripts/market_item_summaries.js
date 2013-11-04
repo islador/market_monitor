5503c7af8639faac426e62e42436b724be2d6e2e
islador <luke.isla@gmail.com>
2013-11-04 12:24:59 -0800

    Stripped out duplicate functions as they were formatted wrong and caused multiple render calls to the server. This reduced functionality and now only station_select fires an AJAX call.

--- app/assets/javascripts/market_item_summaries.js
+++ app/assets/javascripts/market_item_summaries.js
@@ -31,51 +31,6 @@ $(document).ready(function(){
 	    		owner_id: ow, 
 	    		type: ty }
 	    });
-	    //alert("Unobtrusive!" + ss + ls + ow + ty);
-	});
-
-	$("#listing_character").change(function(){
-		var ss = $("#station_select").val();
-		var ls = $("#listing_character").val();
-		var ow = $("#owner").val();
-		var ty = $("#type").val();
-	    $.ajax({
-	    	url: "marketsummaries/filter", type: "GET",
-	    	data: { station_id: ss, 
-	    		listing_character_id: ls, 
-	    		owner_id: ow, 
-	    		type: ty }
-	    });
-	    //alert("Unobtrusive!" + ss + ls + ow + ty);
-	});
-
-	$("#owner").change(function(){
-		var ss = $("#station_select").val();
-		var ls = $("#listing_character").val();
-		var ow = $("#owner").val();
-		var ty = $("#type").val();
-	    $.ajax({
-	    	url: "marketsummaries/filter", type: "GET",
-	    	data: { station_id: ss, 
-	    		listing_character_id: ls, 
-	    		owner_id: ow, 
-	    		type: ty }
-	    });
-	    //alert("Unobtrusive!" + ss + ls + ow + ty);
-	});
-
-	$("#type").change(function(){
-		var ss = $("#station_select").val();
-		var ls = $("#listing_character").val();
-		var ow = $("#owner").val();
-		var ty = $("#type").val();
-	    $.ajax({
-	    	url: "marketsummaries/filter", type: "GET",
-	    	data: { station_id: ss, 
-	    		listing_character_id: ls, 
-	    		owner_id: ow, 
-	    		type: ty }
-	    });
-	    //alert("Unobtrusive!" + ss + ls + ow + ty);
+	    //alert(ss + ls + ow + ty);
 	});
 });
