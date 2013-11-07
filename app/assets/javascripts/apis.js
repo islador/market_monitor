ae7591f5034d6b23112c36901be275822149ed8a
islador <luke.isla@gmail.com>
2013-11-06 23:23:51 -0800

    Designed and implemented an ajax query to fetch the wallet_settings.html.erb partial.

--- app/assets/javascripts/apis.js
+++ app/assets/javascripts/apis.js
@@ -2,19 +2,30 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
+$(document).ready(function(){
+	//When an API row is clicked
+	$("tr[id^='api']").click(function(){
+		//The above line uses jQuery's attributeStartsWith selector to find all <tr> tags with ids starting with 'api'.
 
-var count = 0;
-function countChar()
-{
-	count = document.getElementById("api_v_code").value.length;
-	document.getElementById("counter").innerHTML=count + '/64';
-	document.getElementById("counter").style.color='White';
-	if (count >= 65)
-	{
-		document.getElementById("counter").innerHTML="Verification Code Character Limit Exceeded";
-		document.getElementById("counter").style.color='Red';
-	} else
-	{
-		document.getElementById("counter").style.color='White';
-	}
-}
+		//Extract the value attribute from the clicked on api.
+		var apiid = $("#" + this.id).attr( 'value' );
+		//Extract the array index from the clicked on api.
+		var arrayid = this.id.slice(this.id.length -1, this.id.length)
+		//Check for empty #wsets<x>
+		if ($.trim($("#wsets" + arrayid).html())=='')  {
+			//Fire an AJAX call to apis#wallet_settings
+		    $.ajax({
+		    	url: "apis/wallet_settings", type: "GET",
+		    	//Pass the apiid to the controller as well as the clicked on index.
+		    	data: { api_id: apiid, array_index: arrayid}
+		    });
+		}
+		//Check for non-empty #wsets<x>
+		if ($.trim($("#wsets" + arrayid).html())!='') {
+			//remove all content from #wsets<x>
+			$("#wsets" + arrayid).empty()
+		}
+
+		//This could be improved with a hide() and show() pair, but I was unable to fix the logic loop.
+	});
+});
